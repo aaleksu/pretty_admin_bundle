c03c89d995d982102fe64c29ce35f5b52e0db8bc
Alex <a.aleksu@gmail.com>
2014-08-17 00:48:24 +0700

    improved base controller interface

--- PrettyAdmin/PrettyAdminBundle/Controller/BaseController.php
+++ PrettyAdmin/PrettyAdminBundle/Controller/BaseController.php
@@ -9,15 +9,20 @@ class BaseController extends Controller
 {
     protected $container;
     protected $em;
+    protected $connection;
     protected $entity;
     protected $bundle;
     protected $namespace;
+    protected $entityMetadata;
     protected $prettyAdminParams;
+    protected $repository;
+    protected $entityTableName;
 
     public function setContainer(ContainerInterface $container = null)
     {
         $this->container = $container;
         $this->em = $container->get('doctrine')->getManager();
+        $this->connection = $container->get('database_connection');
         $this->prettyAdminParams = $container->getParameter('pretty_admin');
         $this->initPrettyAdminParams();
     }
@@ -30,6 +35,9 @@ class BaseController extends Controller
             $this->entity = $entityName;
             $this->bundle = $this->prettyAdminParams['entities']['bundle'];
             $this->namespace = $this->prettyAdminParams['entities']['namespace'];
+            $this->repository = sprintf("%s:%s", $this->bundle, $this->entity);
+            $this->entityMetadata = $this->em->getClassMetadata($this->repository);
+            $this->entityTableName = $this->entityMetadata->getTableName();
         }
         else {
             throw new \Exception(sprintf("Entity %s is not registered. Check it out in your config file", $entityName));
